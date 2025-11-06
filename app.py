2998bd6004bd8a502f0463e0b583287f0420a8dc
Muhammad Asim Iqbal <105410266+2021-uam-4619@users.noreply.github.com>
2025-11-06 15:24:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2020,9 +2020,9 @@ def generate_bilty_expense_pdf(payments, start_date, end_date):
     # Payments table
     pdf.set_fill_color(200, 220, 255)
     pdf.cell(15, 10, 'ID', 1, 0, 'C', True)
-    pdf.cell(80, 25, 'Party Name', 1, 0, 'C', True)
+    pdf.cell(80, 10, 'Party Name', 1, 0, 'C', True)
     pdf.cell(35, 10, 'Date', 1, 0, 'C', True)
-    pdf.cell(50, 25, 'Amount', 1, 1, 'C', True)
+    pdf.cell(50, 10, 'Amount', 1, 1, 'C', True)
     
     pdf.set_fill_color(255, 255, 255)
     total_amount = 0
