2a45da2499dccc908e9f23ea6749c35d6f586a34
Muhammad Asim Iqbal <105410266+2021-uam-4619@users.noreply.github.com>
2025-11-06 15:03:23 +0500

    Change width of 'Party Name' column in PDF

--- app.py
+++ app.py
@@ -2023,7 +2023,7 @@ pdf.ln(10)
 # Payments table
 pdf.set_fill_color(200, 220, 255)
 pdf.cell(30, 10, 'ID', 1, 0, 'C', True)
-pdf.cell(100, 10, 'Party Name', 1, 0, 'C', True)
+pdf.cell(80, 10, 'Party Name', 1, 0, 'C', True)
 pdf.cell(50, 10, 'Date', 1, 0, 'C', True)
 pdf.cell(90, 10, 'Remarks', 1, 0, 'C', True)
 pdf.cell(40, 10, 'Amount', 1, 1, 'C', True)
